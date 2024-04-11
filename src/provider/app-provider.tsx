9433bb2c3553656936462214ce3ddeda61310865
phamdatt <tiendatt3009@gmail.com>
2024-04-11 09:26:51 +0700

    remove css unused

--- src/provider/app-provider.tsx
+++ src/provider/app-provider.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import React, { useEffect, useState, Suspense } from "react";
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css";
-import "primereact/resources/primereact.css";
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+
 import { PrimeReactProvider } from "primereact/api";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
