90e893ebb451f7fcd94b41e94a1a81c665ff0b1b
phamdatt <tiendatt3009@gmail.com>
2024-05-24 17:17:07 +0700

    update load api

--- src/app/api/route.ts
+++
@@ -1,6 +0,0 @@
-import { cookies } from "next/headers";
-import { LANGUAGE_COOKIE, Locales } from "@/helpers/i18n/settings";
-export async function GET() {
-  const value = cookies().get(LANGUAGE_COOKIE)?.value;
-  return Response.json({ value: value as Locales });
-}
